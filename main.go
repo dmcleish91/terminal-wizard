c9ccab245c7eb529735fdb35e2eced21ccfcbdac
Dwight McLeish Jr <dmcleishjr@gmail.com>
2025-08-27 22:47:48 -0400

    first commit

---
+++ main.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"log"
+
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+type Styles struct {
+	BorderColor lipgloss.Color
+	InputField  lipgloss.Style
+}
+
+func DefaultStyles() *Styles {
+	s := new(Styles)
+	s.BorderColor = lipgloss.Color("36")
+	s.InputField = lipgloss.NewStyle().
+		BorderForeground(s.BorderColor).
+		BorderStyle(lipgloss.NormalBorder()).
+		Padding(1).
+		Width(80)
+	return s
+}
+
+type model struct {
+	index     int
+	questions []Question
+	width     int
+	height    int
+	styles    *Styles
+}
+
+type Question struct {
+	question string
+	answer   string
+	input    Input
+}
+
+func NewQuestion(question string) Question {
+	return Question{
+		question: question,
+	}
+}
+
+func NewShortQuestion(question string) Question {
+	q := NewQuestion(question)
+	field := NewShortAnswerField()
+	q.input = field
+	return q
+}
+
+func NewLongQuestion(question string) Question {
+	q := NewQuestion(question)
+	field := NewLongAnswerField()
+	q.input = field
+	return q
+}
+
+func New(questions []Question) *model {
+	styles := DefaultStyles()
+	answerField := textinput.New()
+	answerField.Width = 80
+	answerField.Placeholder = "Your answer here"
+	answerField.Focus()
+	return &model{
+		questions: questions,
+		styles:    styles,
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m *model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	current := &m.questions[m.index]
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.width = msg.Width
+		m.height = msg.Height
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		case "enter":
+			current.answer = current.input.Value()
+			log.Printf("question: %s, answer: %s", current.question, current.answer)
+			m.Next()
+			return m, current.input.Blur
+		}
+	}
+	current.input, cmd = current.input.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	current := m.questions[m.index]
+	if m.width == 0 {
+		return "loading..."
+	}
+	// "loading sucessful 🚀"
+	return lipgloss.Place(
+		m.width,
+		m.height,
+		lipgloss.Center,
+		lipgloss.Center,
+		lipgloss.JoinVertical(
+			lipgloss.Center,
+			m.questions[m.index].question,
+			m.styles.InputField.Render(current.input.View()),
+		),
+	)
+}
+
+func (m *model) Next() {
+	if m.index < len(m.questions)-1 {
+		m.index++
+	} else {
+		m.index = 0
+	}
+}
+
+func main() {
+	questions := []Question{
+		NewShortQuestion("What is your name?"),
+		NewShortQuestion("What is your favorite editor?"),
+		NewLongQuestion("What is your favorite qoute?"),
+	}
+	m := New(questions)
+
+	f, err := tea.LogToFile("debug.log", "debug")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	p := tea.NewProgram(m, tea.WithAltScreen())
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
